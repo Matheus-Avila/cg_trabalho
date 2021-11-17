7153a4fe82641cd516ac96dc1ce4bad89be8839f
Matheus-Avila <matheusavila1997@gmail.com>
2021-11-16 23:16:54 -0300

    Carro movendo no plano

---
+++ works/trabalho.js
@@ -0,0 +1,181 @@
+import * as THREE from  '../build/three.module.js';
+import Stats from       '../build/jsm/libs/stats.module.js';
+import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import KeyboardState from '../libs/util/KeyboardState.js';
+import {initRenderer, 
+        initCamera,
+        InfoBox,
+        onWindowResize,
+        createGroundPlaneWired,
+        degreesToRadians} from "../libs/util/util.js";
+
+// var stats = new Stats();          // To show FPS information
+// var scene = new THREE.Scene();    //  Cria cena
+// var renderer = initRenderer();    //  Inicia o render
+// var lookAtVec   = new THREE.Vector3( 0.0, 0.0, 0.0 );
+// var camPosition = new THREE.Vector3( 15, 15, 10 );
+// var upVec       = new THREE.Vector3( 0.0, 1.0, 0.0 );
+// var vcWidth = 400; 
+// var vcHeidth = 300;
+// var camera = new THREE.PerspectiveCamera(45, vcWidth/vcHeidth, 1.0, 20.0);
+//   camera.position.copy(camPosition);
+//   camera.up.copy(upVec);
+//   camera.lookAt(lookAtVec);
+// var keyboard = new KeyboardState();
+
+// var planeGeometry = new THREE.PlaneGeometry(20, 20);
+// var planeMaterial = new THREE.MeshBasicMaterial({
+//     color: "rgba(250, 150, 150)",
+//     side: THREE.DoubleSide,
+// });
+// var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// // add the plane to the scene
+// scene.add(plane); // plano na cena
+
+// // create a camera
+// var holder = new THREE.Object3D ();
+// scene.add(holder); //  na cena
+
+// holder.add(camera);
+
+// scene.add(new THREE.HemisphereLight());
+
+// // Listen window size changes
+// window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+// render();// render executado
+// function render()
+// {
+//   stats.update(); // Update FPS
+//   keyboardUpdate();
+//   requestAnimationFrame(render);
+//   renderer.render(scene, camera) // Render scene
+// }
+
+// function keyboardUpdate() {
+
+//   keyboard.update();
+
+//   if ( keyboard.pressed(",") )     holder.rotateY( .05);
+//   if ( keyboard.pressed(".") )    holder.rotateY(  -.05 );
+//   if ( keyboard.pressed("left") )     holder.rotateZ( -.05);
+//   if ( keyboard.pressed("right") )   holder.rotateZ(  0.05 );
+//   if ( keyboard.pressed("space") )    holder.translateY(  -.05 );
+//   if ( keyboard.pressed("up") )       holder.rotateX(  -.05 );
+//   if ( keyboard.pressed("down") )     holder.rotateX( .05 );
+ 
+// }
+
+var stats = new Stats();          // To show FPS information
+var scene = new THREE.Scene(); 
+var renderer = initRenderer();
+var lookAtVec   = new THREE.Vector3( 0.0, 0.0, 0.0 );
+var camPosition = new THREE.Vector3( 0, -30, 15 );
+var upVec       = new THREE.Vector3( 0.0, -1.0, 0.0 );
+var vcWidth = 400; 
+var vcHeidth = 300;
+// var camera = new THREE.PerspectiveCamera(45, vcWidth/vcHeidth, 0.1, 30.0);
+//   camera.position.copy(camPosition);
+//   camera.up.copy(upVec);
+  // camera.lookAt(lookAtVec);
+var camera = initCamera(new THREE.Vector3(0, -30, 15));
+var keyboard = new KeyboardState();
+
+// Enable mouse rotation, pan, zoom etc.
+var trackballControls = new TrackballControls( camera, renderer.domElement );
+
+var axesHelper = new THREE.AxesHelper( 12 );
+scene.add( axesHelper );
+
+var planeGeometry = new THREE.PlaneGeometry(2000, 2000);
+var planeMaterial = new THREE.MeshBasicMaterial({
+    color: "rgba(150, 150, 150)",
+    side: THREE.DoubleSide,
+});
+var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// add the plane to the scene
+scene.add(plane); 
+
+// create a cube
+var cubeGeometry = new THREE.BoxGeometry(4, 2, 2); 
+var cubeMaterial = new THREE.MeshBasicMaterial({color: "rgba(0, 10, 150)"});
+var carro = new THREE.Mesh(cubeGeometry, cubeMaterial);
+// position the carro
+carro.position.set(0.0, 0.0, 1.5);
+carro.translateX(Math.PI/2);
+// add the carro to the scene
+scene.add(carro);
+
+var eixofG = new THREE.CylinderGeometry( 0.1, 0.1, 2.3, 32 );
+var eixofM = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var eixof = new THREE.Mesh(eixofG, eixofM);
+eixof.position.x = eixof.position.x-1;
+eixof.position.z = eixof.position.z-1;
+carro.add(eixof);
+
+var eixotG = new THREE.CylinderGeometry( 0.1, 0.1, 2.3, 32 );
+var eixotM = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var eixot = new THREE.Mesh(eixotG, eixotM);
+eixot.position.x = eixot.position.x+1;
+eixot.position.z = eixot.position.z-1;
+carro.add(eixot);
+
+var rodaDfG = new THREE.CylinderGeometry( 0.5, 0.5, 0.3, 32 );
+var rodaDfM = new THREE.MeshBasicMaterial( {color: 0x000000} );
+var rodaDf = new THREE.Mesh(rodaDfG, rodaDfM);
+// rodaDf.position.x = rodaDf.position.x+0.5;
+rodaDf.position.y = rodaDf.position.y-1.1;
+// rodaDf.position.z = rodaDf.position.z-0.5;
+eixof.add(rodaDf);
+
+var rodaEfG = new THREE.CylinderGeometry( 0.5, 0.5, 0.3, 32 );
+var rodaEfM = new THREE.MeshBasicMaterial( {color: 0x000000} );
+var rodaEf = new THREE.Mesh(rodaEfG, rodaEfM);
+// rodaEf.position.x = rodaEf.position.x+1;
+rodaEf.position.y = rodaEf.position.y+1.1;
+// rodaEf.position.z = rodaEf.position.z-1;
+eixof.add(rodaEf);
+
+var rodaDtG = new THREE.CylinderGeometry( 0.5, 0.5, 0.3, 32 );
+var rodaDtM = new THREE.MeshBasicMaterial( {color: 0x000000} );
+var rodaDt = new THREE.Mesh(rodaDtG, rodaDtM);
+rodaDt.position.y = rodaDt.position.y-1.1;
+eixot.add(rodaDt);
+
+var rodaEtG = new THREE.CylinderGeometry( 0.5, 0.5, 0.3, 32 );
+var rodaEtM = new THREE.MeshBasicMaterial( {color: 0x000000} );
+var rodaEt = new THREE.Mesh(rodaEtG, rodaEtM);
+rodaEt.position.y = rodaEt.position.y+1.1;
+eixot.add(rodaEt);
+
+// Use this to show information onscreen
+var controls = new InfoBox();
+  controls.add("Basic Scene");
+  controls.addParagraph();
+  controls.add("Use mouse to interact:");
+  controls.add("* Left button to rotate");
+  controls.add("* Right button to translate (pan)");
+  controls.add("* Scroll to zoom in/out.");
+  controls.show();
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+render();// render executado
+function render()
+{
+  stats.update(); // Update FPS
+  trackballControls.update(); // Enable mouse movements
+  keyboardUpdate();
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
+
+function keyboardUpdate() {
+
+  keyboard.update();
+  if ( keyboard.pressed("left") )     carro.rotateZ( .05);
+  if ( keyboard.pressed("right") )   carro.rotateZ(  -0.05 );
+  if ( keyboard.pressed("space") )    carro.translateX(  -.05 );
+ 
+}
