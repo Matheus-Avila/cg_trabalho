661adeb67f8e0595d8d6146e676b4e7f9589fd62
André Vidal <andre.vidal@rockcontent.com>
2021-11-22 11:03:07 -0300

    Imports the Block class and adds blocks iteratively

---
+++ works/classes/block.js
@@ -0,0 +1,19 @@
+import * as THREE from '../../build/three.module.js';
+
+export class Block {
+    constructor(width, height, depth, type, position) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.type = type;
+        this.boxGeometry = new THREE.BoxGeometry(this.width, this.height, this.depth);
+
+        if (type == "initial")
+            this.material = new THREE.MeshPhongMaterial({ color: "rgba(250, 10, 150)" });
+        else
+            this.material = new THREE.MeshPhongMaterial({ color: "rgba(250, 150, 250)" });
+
+        this.mesh = new THREE.Mesh(this.boxGeometry, this.material);
+        this.mesh.position.set(position[0], position[1], position[2]);
+    }
+}
--- works/trabalho.js
+++ works/trabalho.js
@@ -1,195 +1,143 @@
-import * as THREE from  '../build/three.module.js';
-import Stats from       '../build/jsm/libs/stats.module.js';
-import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import * as THREE from '../build/three.module.js';
+import Stats from '../build/jsm/libs/stats.module.js';
+import { TrackballControls } from '../build/jsm/controls/TrackballControls.js';
 import KeyboardState from '../libs/util/KeyboardState.js';
-import {initRenderer, 
-        initCamera,
-        InfoBox,
-        onWindowResize,
-        createGroundPlaneWired,
-        degreesToRadians} from "../libs/util/util.js";
+import {
+  initRenderer,
+  initCamera,
+  InfoBox,
+  onWindowResize,
+  createGroundPlaneWired,
+  degreesToRadians
+} from "../libs/util/util.js";
+import { Block } from './classes/block.js';
 
 // Init
-var stats = new Stats();          
-var scene = new THREE.Scene(); 
+var stats = new Stats();
+var scene = new THREE.Scene();
 var renderer = initRenderer();
 
 // Camera config
-var lookAtVec   = new THREE.Vector3( 0.0, 0.0, 0.0 );
-var camPosition = new THREE.Vector3( -30, -30, 50 );
-var upVec       = new THREE.Vector3( 0.0, 0, 1.0 );
-var vcWidth = 400; 
+var lookAtVec = new THREE.Vector3(0.0, 0.0, 0.0);
+var camPosition = new THREE.Vector3(-30, -30, 50);
+var upVec = new THREE.Vector3(0.0, 0, 1.0);
+var vcWidth = 400;
 var vcHeidth = 300;
-var camera = new THREE.PerspectiveCamera(45, vcWidth/vcHeidth, 0.1, 8000.0);
+var camera = new THREE.PerspectiveCamera(45, vcWidth / vcHeidth, 0.1, 8000.0);
 camera.position.copy(camPosition);
 camera.up.copy(upVec);
 camera.lookAt(lookAtVec);
 var keyboard = new KeyboardState();
-var trackballControls = new TrackballControls( camera, renderer.domElement );
+var trackballControls = new TrackballControls(camera, renderer.domElement);
 
 scene.add(new THREE.HemisphereLight());
 // initDefaultBasicLight(scene, true);
 
-var axesHelper = new THREE.AxesHelper( 12 );
-scene.add( axesHelper );
-
-class bloco {
-  constructor(largura, altura, profundidade, tipo, pos_centro){
-    this.largura = largura;
-    this.altura = altura;
-    this.profundidade = profundidade;
-    this.tipo = tipo;
-    this.forma = new THREE.BoxGeometry(this.altura, this.largura, this.profundidade);
-    if(tipo=="inicial"){ this.material = new THREE.MeshPhongMaterial({color: "rgba(250, 10, 150)"});}
-    else{ this.material = new THREE.MeshPhongMaterial({color: "rgba(0, 150, 250)"});}
-    this.bloc = new THREE.Mesh(this.forma, this.material);
-    this.bloc.position.set(pos_centro[0], pos_centro[1], pos_centro[2]);
-    if(tipo=="inicial") this.passouPeloBloco = true;
-    else this.passouPeloBloco = false;
-  }
-}
-var tamanho_bloco = 9.7;
+var axesHelper = new THREE.AxesHelper(12);
+scene.add(axesHelper);
 
 // Plane Config
 var planeGeometry = new THREE.PlaneGeometry(2000, 2000);
 planeGeometry.translate(0.0, 0.0, -0.3);
 var planeMaterial = new THREE.MeshPhongMaterial({
-    color: "rgba(150, 150, 150)",
-    side: THREE.DoubleSide,
+  color: "rgba(150, 150, 150)",
+  side: THREE.DoubleSide,
 });
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-scene.add(plane); 
+scene.add(plane);
 
-var blocos_comuns = [];
-var bloco_inicial = new bloco(tamanho_bloco,tamanho_bloco,0.3,"inicial",[0,0,-0.1])
-scene.add(bloco_inicial.bloc);
-blocos_comuns.push(bloco_inicial);
+var blockSize = 9.7;
+var blockDepth = 0.3;
+var commonBlocks = [];
+var commonBlockPositions = [
+  [10, 0, -0.1],
+  [20, 0, -0.1],
+  [-10, 0, -0.1],
+  [-20, 0, -0.1],
+  [-20, 10, -0.1],
+  [-20, 20, -0.1],
+  [-20, 30, -0.1],
+  [-20, 40, -0.1],
+  [-10, 40, -0.1],
+  [0, 40, -0.1],
+  [10, 40, -0.1],
+  [20, 40, -0.1],
+  [20, 30, -0.1],
+  [20, 20, -0.1],
+  [20, 10, -0.1],
+  [20, 0, -0.1]
+]
 
-//Blocos reta inicial
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[10,0,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
+var initialBlock = new Block(blockSize, blockSize, blockDepth, "initial", [0, 0, -0.1])
+scene.add(initialBlock.mesh);
+commonBlocks.push(initialBlock);
 
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[20,0,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[-10,0,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[-20,0,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-// Blocos curva direita
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[-20,10,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[-20,20,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[-20,30,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[-20,40,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[-10,40,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[0,40,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[10,40,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[20,40,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[20,30,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[20,20,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[20,10,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
-
-var bloco_comum = new bloco(tamanho_bloco,tamanho_bloco,0.3,"comum",[20,0,-0.1]);
-blocos_comuns.push(bloco_comum);
-scene.add(bloco_comum.bloc);
+for (let i = 0; i < commonBlockPositions.length; i++) {
+  var commonBlock = new Block(blockSize, blockSize, blockDepth, "common", commonBlockPositions[i]);
+  commonBlocks.push(commonBlock);
+  scene.add(commonBlock.mesh);
+}
 
 // Car Config
-var cubeGeometry = new THREE.BoxGeometry(4, 2, 2); 
-var cubeMaterial = new THREE.MeshPhongMaterial({color: "rgba(0, 10, 150)"});
+var cubeGeometry = new THREE.BoxGeometry(4, 2, 2);
+var cubeMaterial = new THREE.MeshPhongMaterial({ color: "rgba(0, 10, 150)" });
 var carro = new THREE.Mesh(cubeGeometry, cubeMaterial);
 carro.position.set(0.0, 0.0, 1.5);
 scene.add(carro);
 
-var eixofG = new THREE.CylinderGeometry( 0.1, 0.1, 2.3, 32 );
-var eixofM = new THREE.MeshPhongMaterial( {color: 0xffff00} );
+var eixofG = new THREE.CylinderGeometry(0.1, 0.1, 2.3, 32);
+var eixofM = new THREE.MeshPhongMaterial({ color: 0xffff00 });
 var eixof = new THREE.Mesh(eixofG, eixofM);
-eixof.position.x = eixof.position.x-1;
-eixof.position.z = eixof.position.z-1;
+eixof.position.x = eixof.position.x - 1;
+eixof.position.z = eixof.position.z - 1;
 carro.add(eixof);
 
-var eixotG = new THREE.CylinderGeometry( 0.1, 0.1, 2.3, 32 );
-var eixotM = new THREE.MeshPhongMaterial( {color: 0xffff00} );
+var eixotG = new THREE.CylinderGeometry(0.1, 0.1, 2.3, 32);
+var eixotM = new THREE.MeshPhongMaterial({ color: 0xffff00 });
 var eixot = new THREE.Mesh(eixotG, eixotM);
-eixot.position.x = eixot.position.x+1;
-eixot.position.z = eixot.position.z-1;
+eixot.position.x = eixot.position.x + 1;
+eixot.position.z = eixot.position.z - 1;
 carro.add(eixot);
 
-var rodaDfG = new THREE.CylinderGeometry( 0.5, 0.5, 0.3, 32 );
-var rodaDfM = new THREE.MeshPhongMaterial( {color: 0x000000} );
+var rodaDfG = new THREE.CylinderGeometry(0.5, 0.5, 0.3, 32);
+var rodaDfM = new THREE.MeshPhongMaterial({ color: 0x000000 });
 var rodaDf = new THREE.Mesh(rodaDfG, rodaDfM);
 // rodaDf.position.x = rodaDf.position.x+0.5;
-rodaDf.position.y = rodaDf.position.y-1.1;
+rodaDf.position.y = rodaDf.position.y - 1.1;
 // rodaDf.position.z = rodaDf.position.z-0.5;
 eixof.add(rodaDf);
 
-var rodaEfG = new THREE.CylinderGeometry( 0.5, 0.5, 0.3, 32 );
-var rodaEfM = new THREE.MeshPhongMaterial( {color: 0x000000} );
+var rodaEfG = new THREE.CylinderGeometry(0.5, 0.5, 0.3, 32);
+var rodaEfM = new THREE.MeshPhongMaterial({ color: 0x000000 });
 var rodaEf = new THREE.Mesh(rodaEfG, rodaEfM);
 // rodaEf.position.x = rodaEf.position.x+1;
-rodaEf.position.y = rodaEf.position.y+1.1;
+rodaEf.position.y = rodaEf.position.y + 1.1;
 // rodaEf.position.z = rodaEf.position.z-1;
 eixof.add(rodaEf);
 
-var rodaDtG = new THREE.CylinderGeometry( 0.5, 0.5, 0.3, 32 );
-var rodaDtM = new THREE.MeshPhongMaterial( {color: 0x000000} );
+var rodaDtG = new THREE.CylinderGeometry(0.5, 0.5, 0.3, 32);
+var rodaDtM = new THREE.MeshPhongMaterial({ color: 0x000000 });
 var rodaDt = new THREE.Mesh(rodaDtG, rodaDtM);
-rodaDt.position.y = rodaDt.position.y-1.1;
+rodaDt.position.y = rodaDt.position.y - 1.1;
 eixot.add(rodaDt);
 
-var rodaEtG = new THREE.CylinderGeometry( 0.5, 0.5, 0.3, 32 );
-var rodaEtM = new THREE.MeshPhongMaterial( {color: 0x000000} );
+var rodaEtG = new THREE.CylinderGeometry(0.5, 0.5, 0.3, 32);
+var rodaEtM = new THREE.MeshPhongMaterial({ color: 0x000000 });
 var rodaEt = new THREE.Mesh(rodaEtG, rodaEtM);
-rodaEt.position.y = rodaEt.position.y+1.1;
+rodaEt.position.y = rodaEt.position.y + 1.1;
 eixot.add(rodaEt);
 
-carro.scale.set(0.4,0.4,.4)
+carro.scale.set(0.4, 0.4, .4)
 
-function verificaPista(carro){
-  for(var i = 0; i<blocos_comuns.length; i++){
-    if(carro.position.x <= blocos_comuns[i].bloc.position.x+tamanho_bloco*1.1/2 && 
-      carro.position.x >= blocos_comuns[i].bloc.position.x-tamanho_bloco*1.1/2 &&
-      carro.position.y <= blocos_comuns[i].bloc.position.y+tamanho_bloco*1.1/2 &&
-      carro.position.y >= blocos_comuns[i].bloc.position.y-tamanho_bloco*1.1/2 ){
-        console.log("carro: ", carro.position.x, " bloco", i, ", carro: ", carro.position.y);
-        return true;
-      }
+function verificaPista(carro) {
+  for (var i = 0; i < commonBlocks.length; i++) {
+    if (carro.position.x <= commonBlocks[i].mesh.position.x + blockSize * 1.1 / 2 &&
+      carro.position.x >= commonBlocks[i].mesh.position.x - blockSize * 1.1 / 2 &&
+      carro.position.y <= commonBlocks[i].mesh.position.y + blockSize * 1.1 / 2 &&
+      carro.position.y >= commonBlocks[i].mesh.position.y - blockSize * 1.1 / 2) {
+      console.log("carro: ", carro.position.x, " bloco", i, ", carro: ", carro.position.y);
+      return true;
+    }
   }
   return false;
 }
@@ -200,56 +148,56 @@ var speed_max = 0.5;
 
 // Use this to show information onscreen
 var controls = new InfoBox();
-  controls.add("Basic Scene");
-  controls.addParagraph();
-  controls.add("Use mouse to interact:");
-  controls.add("* Left button to rotate");
-  controls.add("* Right button to translate (pan)");
-  controls.add("* Scroll to zoom in/out.");
-  controls.show();
+controls.add("Basic Scene");
+controls.addParagraph();
+controls.add("Use mouse to interact:");
+controls.add("* Left button to rotate");
+controls.add("* Right button to translate (pan)");
+controls.add("* Scroll to zoom in/out.");
+controls.show();
 
 // Listen window size changes
-window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
 
 // Controls Config
 function keyboardUpdate() {
-  keyboard.update(); 
-  if ( keyboard.pressed("left") ){     
-    carro.rotateZ( .05);
+  keyboard.update();
+  if (keyboard.pressed("left")) {
+    carro.rotateZ(.05);
   }
-  if ( keyboard.pressed("right") ){
-    carro.rotateZ( -.05);
+  if (keyboard.pressed("right")) {
+    carro.rotateZ(-.05);
   }
-  if ( keyboard.pressed("X") ){
-    if(speed< speed_max){
+  if (keyboard.pressed("X")) {
+    if (speed < speed_max) {
       speed = speed + 0.01;
     }
-    if(!verificaPista(carro)){
-      speed = speed*0.5;
+    if (!verificaPista(carro)) {
+      speed = speed * 0.5;
     }
     carro.translateX(speed);
     // var x = eixof.axis;
     // rotateAroundObjectAxis(eixot, )
     // carro.children[0].translateX(  -speed );
   }
-  if ( !keyboard.pressed("X") ){
-    if(speed> 0){
-      if(!verificaPista(carro)){
-        speed = speed*0.5;
+  if (!keyboard.pressed("X")) {
+    if (speed > 0) {
+      if (!verificaPista(carro)) {
+        speed = speed * 0.5;
       }
       speed = speed - 0.01;
-      carro.translateX(  speed );
+      carro.translateX(speed);
       // carro.rotateOnAxis(, 0,05);
       // p2.matrixWorld.makeRotationAxis(axis, Math.random());
     }
   }
-  if (keyboard.pressed("down")){
-    if(!verificaPista(carro)){
-      speed = speed*0.5;
+  if (keyboard.pressed("down")) {
+    if (!verificaPista(carro)) {
+      speed = speed * 0.5;
     }
-    if(speed> 0){
-      speed = .95*speed;
-    }else{
+    if (speed > 0) {
+      speed = .95 * speed;
+    } else {
       carro.translateX(-.08);
     }
   }
@@ -257,8 +205,7 @@ function keyboardUpdate() {
 
 render();
 
-function render()
-{
+function render() {
   stats.update(); // Update FPS
   trackballControls.update(); // Enable mouse movements
   keyboardUpdate();
