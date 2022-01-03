04a32c18a5e472b2a931b965d7b4e0912a4ff1cc
Matheus-Avila <matheusavila1997@gmail.com>
2022-01-03 12:22:11 -0300

    creating car

--- works/builders/carBuilder.js
+++ works/builders/carBuilder.js
@@ -1,28 +1,67 @@
 import { Car } from "../classes/car.js";
 import * as THREE from '../../build/three.module.js';
 import { degreesToRadians } from '../../libs/util/util.js';
+import {ConvexGeometry} from '../../build/jsm/geometries/ConvexGeometry.js';
+
+var colorCasing = {color:"rgb(150,150,150)"};
 
 var buildCar = function (scene, maxSpeed) {
     var maxSpeed = 0.3;
     var maxAngleAxle = 0.3;
-    var casing = buildCasing();
+    var casing = buildCasing(scene);
+    
     var axle = buildAxle(casing);
     buildWheels(axle.front, axle.back);
     scene.add(casing);
-
     return new Car(casing, maxSpeed, maxAngleAxle);
 }
 
-var buildCasing = function () {
-    var casingGeometry = new THREE.CylinderGeometry(1.0, 1.3, 2.5, 40, 40, false, 1);
-    casingGeometry.rotateZ(degreesToRadians(270));
-    var casingMaterial = new THREE.MeshPhongMaterial({ color: "rgba(128, 0, 0)" });
-    var casing = new THREE.Mesh(casingGeometry, casingMaterial);
-    casing.position.set(0.0, 0.0, .63);
-    casing.scale.set(0.4, 0.4, 0.4);
-    casing.rotation.set(0, 0, degreesToRadians(180));
+// var buildCasing = function () {
+//     var casingGeometry = new THREE.CylinderGeometry(1.0, 1.3, 2.5, 40, 40, false, 1);
+//     casingGeometry.rotateZ(degreesToRadians(270));
+//     var casingMaterial = new THREE.MeshPhongMaterial({ color: "rgba(128, 0, 0)" });
+//     var casing = new THREE.Mesh(casingGeometry, casingMaterial);
+//     casing.position.set(0.0, 0.0, .63);
+//     casing.scale.set(0.4, 0.4, 0.4);
+//     casing.rotation.set(0, 0, degreesToRadians(180));
 
-    return casing;
+//     return casing;
+// }
+
+var buildCasing = function (scene)
+{
+  var points = [];
+  points.push(new THREE.Vector3(3,1,1));
+  points.push(new THREE.Vector3(3,-1,1));
+  points.push(new THREE.Vector3(-3,1,1));
+  points.push(new THREE.Vector3(-3,-1,1));
+  points.push(new THREE.Vector3(3,1,-1));
+  points.push(new THREE.Vector3(3,-1,-1));
+  points.push(new THREE.Vector3(-3,1,-1));
+  points.push(new THREE.Vector3(-3,-1,-1));
+  points.push(new THREE.Vector3(0,-1,2));
+  points.push(new THREE.Vector3(0,1,2));
+
+  var material = new THREE.MeshPhongMaterial({color:"rgb(255,255,255)"});
+
+  var pointCloud = new THREE.Object3D();  
+  points.forEach(function (point) {
+    var spGeom = new THREE.SphereGeometry(0.2);
+    var spMesh = new THREE.Mesh(spGeom, material);
+    spMesh.position.set(0, 0, .63);
+    pointCloud.add(spMesh);
+  });
+
+  scene.add(pointCloud);
+
+  pointCloud.visible = true;
+  var convexCasing = new ConvexGeometry(points);
+  var casing = new THREE.MeshPhongMaterial(colorCasing);
+  var objectcasing = new THREE.Mesh(convexCasing, casing);
+      objectcasing.castShadow = true;
+      objectcasing.visible = true;
+    objectcasing.position.z = objectcasing.position.z + 1;
+  return objectcasing;
 }
 
 var buildAxle = function (car) {
