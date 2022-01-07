c8466dc7fc2ec3deb7234d39c11d178e607997c9
Bernardo <bsacruz@gmail.com>
2022-01-07 14:07:20 -0300

    minimap fix and resized tracks

--- works/builders/cameraBuilderMap.js
+++ works/builders/cameraBuilderMap.js
@@ -7,7 +7,7 @@ var buildCameraMap = function () {
     var upVec = new THREE.Vector3(1.0, 0, 1.0);
     var lookAtVec = new THREE.Vector3(0.0, 0.0, 0.0);
 
-    var camera = new THREE.PerspectiveCamera(50, vcWidth / vcHeidth, 0.1, 8000.0);
+    var camera = new THREE.PerspectiveCamera(80, vcWidth / vcHeidth, 0.1, 8000.0);
     camera.position.copy(camPosition);
     camera.up.copy(upVec);
     camera.lookAt(lookAtVec);
