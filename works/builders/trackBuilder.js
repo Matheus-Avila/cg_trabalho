505c7ed2f7196d70b9dd084fe1d6e71b9470c2dc
Bernardo <bsacruz@gmail.com>
2022-01-08 13:00:05 -0300

    Added new track without jumps

--- works/builders/trackBuilder.js
+++ works/builders/trackBuilder.js
@@ -5,6 +5,10 @@ var buildTrack = function (scene, trackNumber) {
         return buildFirstTrack(scene);
     else if (trackNumber == 2)
         return buildSecondTrack(scene);
+    else if (trackNumber == 3)
+        return buildThirdTrack(scene);
+    else if (trackNumber == 4)
+        return buildFourthTrack(scene);
 }
 
 var buildFirstTrack = function (scene) {
@@ -105,4 +109,108 @@ var buildSecondTrack = function (scene) {
     return track;
 }
 
-export { buildTrack, buildFirstTrack, buildSecondTrack };
+var buildThirdTrack = function (scene) {
+    var trackNumber = 3;
+    var initialBlockPosition =  [20, 60, -0.1];
+    var commonBlockPositions = [
+        [30, -30, -0.1],
+        [20, -30, -0.1],
+        [10, -30, -0.1],
+        [0, -30, -0.1],
+        [-10, -30, -0.1],
+        [-20, -30, -0.1],
+        [-30, -30, -0.1],
+        [-40, -30, -0.1],
+        [-50, -30, -0.1],
+        [-50, -20, -0.1],
+        [-50, -10, -0.1],
+        [-50, 0, -0.1],
+        [-50, 10, -0.1],
+        [-50, 20, -0.1],
+        [-20, 50, -0.1],
+        [-20, 40, -0.1],
+        [-20, 30, -0.1],
+        [-20, 20, -0.1],
+        [-30, 20, -0.1],
+        [-40, 20, -0.1],
+        [-20, 60, -0.1],
+        [-10, 60, -0.1],
+        [0, 60, -0.1],
+        [10, 60, -0.1],
+        [30, 60, -0.1],
+        [40, 60, -0.1],
+        [40, 50, -0.1],
+        [40, 40, -0.1],
+        [10, 20, -0.1],
+        [10, 10, -0.1],
+        [10, 0, -0.1],
+        [10, -10, -0.1],
+        [10, -20, -0.1],
+        [10, 30, -0.1],
+        [20, 30, -0.1],
+        [30, 30, -0.1], 
+        [40, 30, -0.1],
+        [40, 20, -0.1], 
+        [40, 10, -0.1], 
+        [40, 0, -0.1],
+        [40, -10, -0.1],
+        [40, -20, -0.1],
+        [40, -30, -0.1]
+    ];
+    var blockSize = 9.7;
+    var blockDepth = 0.3;
+
+    var track = new Track(trackNumber, initialBlockPosition, commonBlockPositions, blockSize, blockDepth);
+    scene.add(track.group);
+
+    return track;
+}
+
+var buildFourthTrack = function (scene) {
+    var trackNumber = 4;
+    var initialBlockPosition =  [-50, 0, -0.1];
+    var commonBlockPositions = [
+        [-10, 10, -0.1],
+        [-10, 0, -0.1],
+        [-10, -10, -0.1],
+        [-10, -20, -0.1],
+        [-10, -30, -0.1],
+        [-20, -30, -0.1],
+        [-30, -30, -0.1],
+        [-40, -30, -0.1],
+        [-50, -30, -0.1],
+        [-50, -20, -0.1],
+        [-50, -10, -0.1],
+        [-50, 10, -0.1],
+        [0, 30, -0.1],
+        [-10, 30, -0.1],
+        [-10, 20, -0.1],
+        [-20, 10, -0.1],
+        [-30, 10, -0.1],
+        [-40, 10, -0.1],
+        [0, 10, -0.1],
+        [10, 10, -0.1],
+        [10, 20, -0.1],
+        [10, 40, -0.1],
+        [10, 50, -0.1],
+        [10, 60, -0.1],
+        [20, 60, -0.1],
+        [30, 60, -0.1],
+        [40, 60, -0.1],
+        [40, 50, -0.1],
+        [40, 40, -0.1],
+        [40, 30, -0.1],
+        [30, 30, -0.1],
+        [20, 30, -0.1],
+        [10, 30, -0.1]
+    ];
+    var blockSize = 9.7;
+    var blockDepth = 0.3;
+
+    var track = new Track(trackNumber, initialBlockPosition, commonBlockPositions, blockSize, blockDepth);
+    scene.add(track.group);
+
+    return track;
+}
+
+export { buildTrack, buildFirstTrack, buildSecondTrack, buildThirdTrack, buildFourthTrack };
--- works/controls/trackControls.js
+++ works/controls/trackControls.js
@@ -2,19 +2,26 @@ import * as TrackBuilder from '../builders/trackBuilder.js';
 import { degreesToRadians } from '../../libs/util/util.js';
 
 var updateTrack = function (keyboardState, scene, track, car, timer) {
-    if (keyboardState.down("1") && track.number == 2)
+    if (keyboardState.down("1") && track.number != 1)
         track = changeTrack(scene, track, car, 1, timer);
-    else if (keyboardState.down("2") && track.number == 1)
+    else if (keyboardState.down("2") && track.number != 2)
         track = changeTrack(scene, track, car, 2, timer);
-
+    else if (keyboardState.down("3") && track.number != 3)
+        track = changeTrack(scene, track, car, 3, timer);
+    else if (keyboardState.down("4") && track.number != 4)
+        track = changeTrack(scene, track, car, 4, timer);
     return track;
 }
 
 var changeTrack = function (scene, track, car, newTrackNumber, timer) {
     scene.remove(track.group);
     track = TrackBuilder.buildTrack(scene, newTrackNumber);
+    
+    if(newTrackNumber == 4)
+        car.mesh.rotation.set(0, 0, degreesToRadians(90));
+    else
+        car.mesh.rotation.set(0, 0, degreesToRadians(180));
 
-    car.mesh.rotation.set(0, 0, degreesToRadians(180));
     car.mesh.position.setX(track.initialBlockPosition[0]);
     car.mesh.position.setY(track.initialBlockPosition[1]);
     car.speed = 0;
